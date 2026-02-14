2d706516828fa8c805d5dbd6e18ed941caccf34f
Ali Aliyev <alialiyev423@gmail.com>
2026-02-14 19:08:11 +0400

    changes

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = import.meta.env.VITE_API_URL || "https://render-0-q5lr.onrender.com";
+const URL = import.meta.env.VITE_API_URL || "https://chat-app-2-1mdy.onrender.com";
 
 export const socket = io(URL, {
   withCredentials: true,
